f8bcc8370c6271d74d6b9573cef803a6dcf20476
kunjkanani <kunjkanani27@gmail.com>
2021-05-03 20:16:49 +0530

    Project structure added to dev

---
+++ autofy_warranty_app/lib/pages/homepageController.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class HomePageController extends GetxController {
+  var counter = 0.obs;
+
+  void increment() {
+    counter.value++;
+  }
+}
---
+++ autofy_warranty_app/lib/utils/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppTexts {
+  static final double primaryHeadingTextSize = 35.0;
+  static final double secondaryHeadingTextSize = 30.0;
+  static final double normalTextSize = 22.0;
+}
+
+class AppColors {
+  static final Color primaryColor = Color.fromRGBO(216, 57, 57, 1);
+  static final Color secondaryColor = Colors.white;
+  static final Color greyTextColor = Colors.grey;
+  static final Color lightGreyTextColor = Colors.grey.shade300;
+}
